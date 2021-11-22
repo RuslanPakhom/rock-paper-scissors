5f705682e43b2292ac9cb47cb18c266ecfc937e3
Ruslan <sich92@yandex.ru>
2021-11-23 01:05:22 +0300

    Stable UI version

--- script.js
+++ script.js
@@ -1,93 +1,126 @@
-function computerPlay(){
-    let items=['rock','paper','scissors']
-    let rand = Math.floor(Math.random()*3)
-    return items[rand]
+let computer = 0;
+let player = 0;
+//UI
+const playerScore = document.querySelector('#player-score')
+const computerScore = document.querySelector('#computer-score')
+const buttons = document.querySelectorAll('.buttons button')
+const roundContainer = document.querySelector('.round')
+
+//Events
+    buttons.forEach((button) => {
+        button.addEventListener('click',() => {
+            playRound(computerPlay(),button.id);
+        })
+    })
+
+function toggleButtons(){ //Rock Paper Scissors button enabling/disabling
+    
+    buttons.forEach((button) => {
+        button.disabled = !button.disabled
+    })
+
 }
 
-function playerSelection(){
-    let playerChoose='';
-    while(playerChoose!=="rock" && playerChoose!=="paper" && playerChoose!=="scissors"){
-    playerChoose = prompt("Please choose rock/paper/scissors?")
-    playerChoose = playerChoose.toLowerCase()
-}
-    return playerChoose;
-}
+function newGame(){//Start game function. Triggered if new game button clicked
+   
+
+    const startGame = document.querySelector('#start')
+    startGame.addEventListener('click',() => { //new game button onClick listemer
+        toggleButtons()
+        computer = 0
+        player = 0
+        playerScore.textContent = player;
+        computerScore.textContent = computer;       
+        console.log(roundContainer.classList)
+        roundContainer.classList.remove('round-active')
+        console.log(roundContainer.classList)
+    
+        while(roundContainer.firstChild){
+            console.log(roundContainer.removeChild(roundContainer.firstChild))
+        }
+
+       const divRound = document.createElement('div')
+        divRound.classList.add('status-text')
+        divRound.textContent = "Game started! Please choose your weapon!"
+        roundContainer.appendChild(divRound)
+    })
+    }
+
+    function computerPlay(){
+        const items=['rock','paper','scissors']
+        const rand = Math.floor(Math.random()*3)
+        return items[rand]
+    }
 
 function playRound(computerChoose,playerChoose){
 
-    console.log("Computer choose: " + computerChoose);
-    console.log("Your choose: " + playerChoose)
-
+//UI: Round section fillin with round results
+    const playerChooseImage = document.createElement('img')
+    const computerChooseImage = document.createElement('img')
+    const divRound = document.createElement('div')
+    playerChooseImage.src = `assets/${playerChoose}.png`
+    playerChooseImage.alt=`${playerChoose}`
+    computerChooseImage.src = `assets/${computerChoose}.png`
+    computerChooseImage.alt=`${computerChoose}`
+    divRound.classList.add('status-text')
+//UI: removing previous round container
+    while(roundContainer.firstChild){
+        roundContainer.removeChild(roundContainer.firstChild)
+    }
+//UI: adding round results to the page
+    roundContainer.appendChild(playerChooseImage);
+    roundContainer.appendChild(divRound);
+    roundContainer.appendChild(computerChooseImage)
+    roundContainer.classList.add('round-active')
+  
+//Calculating score and adding status text to the page
     if(computerChoose === playerChoose){
-        return 0
+        divRound.textContent = `Draw!`
     }
 
-    if((computerChoose === 'rock' && playerChoose === 'scissors') || 
-    (computerChoose === 'paper' && playerChoose === 'rock') || 
-    (computerChoose === 'scissors' && playerChoose === 'paper')){
-        return -1
+    else if((computerChoose === 'rock' && playerChoose === 'scissors') || 
+                (computerChoose === 'paper' && playerChoose === 'rock') || 
+                (computerChoose === 'scissors' && playerChoose === 'paper')){
+        divRound.textContent = 'You lost the round!'
+        computerScore.textContent = ++computer;
     }
-    return 1
+    else{
+    divRound.textContent = 'You win the round!'
+    playerScore.textContent = ++player;
+    }
+
+    endGameCheck(computer,player)
 }
 
-function logRound(computerChoose, playerChoose){
-    let roundResult = playRound(computerChoose,playerChoose);
-    if(roundResult === 0){
-        console.log(`Draw! Both players chose ${playerChoose}`)
-        return 0
+function endGameCheck(computer,player){//Stop the game if someone gets 5 ponts
+
+    if(computer === 5){
+        const divRound = document.querySelector('div.status-text')
+        divRound.textContent = "You lost the game :("
+        toggleButtons()
+        addNewGame()
     }
-    if(roundResult === 1){
-        console.log(`You won! ${playerChoose} beats ${computerChoose}`);
-        return 1
+
+    if(player === 5){
+        const divRound = document.querySelector('div.status-text')
+        divRound.textContent = "You win the game!"
+        toggleButtons()
+        addNewGame()
     }
-    if(roundResult === -1){
-        console.log(`You lost! ${computerChoose} beats ${playerChoose}`);
-        return -1
-    }
+
 }
 
-function whoWon(player,computer){
-    if(player === computer){
-        return 0
-    }
-    if (player > computer){
-        return 1
-    }
-        return -1;
+function addNewGame(){ //Adding new game button
+    const roundDiv = document.querySelector('.status-text')
+    const newGameBtn = document.createElement('button');
+    newGameBtn.id = "start";
+    newGameBtn.textContent = "New game"
+    roundDiv.appendChild(newGameBtn)
+    newGame()
 }
 
-function game(){
-    let computer = 0;
-    let player = 0;
-    let computerChoose
-    let playerChoose
-    for(let i=0; i<5; i++){
-        console.log(`=======Round ${i+1}========`);
-        computerChoose = computerPlay();
-        playerChoose = playerSelection();
-        let roundResult = logRound(computerChoose,playerChoose);
-        if(roundResult === 1){
-            player++;
-        }
-        if(roundResult === -1){
-            computer++;
-        }
-        console.log("Total score:")
-        console.log(`Player - Computer ${player}:${computer}`)
-    }
+newGame()
 
-    let winner = whoWon(player,computer);
-    console.log("========GAME RESULT==========")
-    if(winner === 0){      
-        console.log(`Draw! ${player}:${computer}`)
-    }
-    if(winner === 1){      
-        console.log(`You WON the game! ${player}:${computer}`)
-    }
-    if (winner === -1){    
-            console.log(`You LOST the game! ${player}:${computer}`)
-    }
-    
-}
 
-game();
+
+
